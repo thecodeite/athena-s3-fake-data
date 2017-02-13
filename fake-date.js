d50b02e7142fec301b9f489dc23ff05001829d7e
Sam Plews <the@codeite.com>
2017-02-13 08:52:25 +0000

    multi

--- fake-date.js
+++ fake-date.js
@@ -9,14 +9,14 @@ const s3 = new AWS.S3()
 const date = moment()
 
 
-createMany(250000, () => {
+createMany(25000, () => {
   console.log('Done!')
 })
 
 function createMany (i, done) {
   createJson(i, date)
-    .then(() => {
-      i--
+    .then(nextI => {
+      i = nextI
       date.add(-1, 'hours')
 
       // if (i > 249990) {
@@ -30,26 +30,29 @@ function createMany (i, done) {
     .catch(err => console.log(err))
 }
 
-
-
 function createJson(i, date) {
   return new Promise((resolve, reject) => {
-    const id = `0000000${i}`.slice(-7)
-    const lastUpdated = date.format('YYYY-MM-DD hh:mm:ss')
+    let document = ''
+    for(let j=0; j<10; j++) {
+      const id = `0000000${i}`.slice(-7)
+      i++
+      const lastUpdated = date.format('YYYY-MM-DD hh:mm:ss')
 
-    const payload = {}
-    for (let key=0; key<50; key++) {
-      payload['key'+key] = randomWords({ min: 300, max: 1000 }).join(' ')
+      const payload = {}
+      for (let key=0; key<50; key++) {
+        payload['key'+key] = randomWords({ min: 300, max: 1000 }).join(' ')
+      }
+
+      const data = {id, lastUpdated, payload: JSON.stringify(payload)}
+      document += JSON.stringify(data) + '\n'
     }
 
-    const data = {id, lastUpdated, payload: JSON.stringify(payload)}
-
-    const filename = `spike2/trial_v1_${id}.json`
-    const params = {Bucket: 'iph-pharma-api-spike', Key: filename, Body: JSON.stringify(data)};
+    const filename = `spike3/trial_v1_${id}.json`
+    const params = {Bucket: 'iph-pharma-api-spike', Key: filename, Body: document};
     s3.putObject(params, function(err, data) {
       if (err) return reject(err)
       console.log('Uploaded:', filename)
-      resolve()
+      resolve(i)
     })
   })
 }
